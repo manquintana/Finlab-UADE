184124f1304693a0fb2e06f5525c70cad6a48b68
Manuel Alejandro Quintana <manuelalejandroquintana@gmail.com>
2022-10-12 17:02:32 -0300

    Updated selenium example

--- Notebooks/14 - Selenium 3 - Interactions.py
+++ Notebooks/14 - Selenium 3 - Interactions.py
@@ -1,13 +1,41 @@
+# These days, most of the web apps are using AJAX techniques. 
+# When a page is loaded by the browser, the elements within that page may load at different time intervals.
+# This makes locating elements difficult: if an element is not yet present in the DOM, a locate function will 
+# raise an ElementNotVisibleException exception. 
+# Using waits, we can solve this issue. 
+# Waiting provides some slack between actions performed - mostly locating an element or any other operation with the element.
+#
+# Y lo que no queremos es justamente "explicitar" el timeout, lo que podríamos hacer sin problemas...
+# Sino que queremos seleccionar un elemento una vez que esté disponible
+#
+# Nuestro objetivo en este ejercicio será posicionar el mapa de Google en Madrid de manera automatizada
+# Pensemos los pasos necesarios:
+# 1. Abrir Google Earth
+# 2. Acceder al cuadro de búsqueda
+# 3. Escribir "Madrid"
+# 4. Confirmar la búsqueda
+
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-url = 'https://www.google.com/earth/'
+import time
+url = 'https://earth.google.com/web/'
 
-driver = webdriver.Chrome('Introducir_ruta_al_driver_correspondiente\chromedriver.exe')
+driver = webdriver.Chrome('Introducir ruta al driver aca')
 driver.get(url)
-wait = WebDriverWait(driver, 10)
-#launchEarthButton = wait.until(EC.element_to_be_clickable((By.XPATH,'/html/body/header/div/nav[1]/ul[2]/li[2]/a/span/span')))
-search_button = wait.until(EC.element_to_be_clickable((By.XPATH,'//*[@id="icon"]')))
 
-search_button.click()
+#search = WebDriverWait(driver,15).until(
+#    EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/div[2]/div/div[3]/div/a/span/span'))
+#    )
+#search.click()
+# ESTO YA NO FUNCIONA, GOOGLE EARTH HIZO ESTE ELEMENTO "NO INTERACTUABLE"!! HAY QUE BUSCAR OTRA MANERA
+
+#search.send_keys('Madrid')
+#search.send_keys(Keys.Enter)
+
+
+
+
+
